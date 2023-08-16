4aba53ab93d7dd26c317880d2986bb9dd2a939d7
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-08-16 17:44:02 +0330

    moved: user controller methods to repo

--- backend/src/app/controllers/user.py
+++ backend/src/app/controllers/user.py
@@ -1,27 +1,8 @@
 from src.core.controller import BaseController
-from src.core.handlers import PasswordHandler
 
 
 class UserController(BaseController):
     """
     User controller is responsible for handling user CRUD operations.
     """
-
-    async def create(self, **data):
-        # Hash password before creating user
-        hashed_password = await PasswordHandler.hash_password(
-            data.pop('password')
-        )
-        data.setdefault('password', hashed_password)
-        return await super().create(**data)
-
-    async def update(self, instance, **data):
-        password = data.get('password')
-
-        if password and not password == instance.password:
-            hashed_password = await PasswordHandler.hash_password(
-                password
-            )
-            data['password'] = hashed_password
-
-        return await super().update(instance, **data)
+    pass
--- backend/src/app/repositories/user.py
+++ backend/src/app/repositories/user.py
@@ -1,3 +1,4 @@
+from src.core.handlers import PasswordHandler
 from src.core.repository import BaseRepository
 
 
@@ -5,4 +6,23 @@ class UserRepository(BaseRepository):
     """
     User repository is responsible for handling user CRUD operations.
     """
-    pass
+
+    async def create(self, **data):
+        # Hash password before creating user
+        hashed_password = await PasswordHandler.hash_password(
+            data.pop('password')
+        )
+        data.setdefault('password', hashed_password)
+        return await super().create(**data)
+
+    async def update(self, instance, **data):
+        password = data.get('password')
+
+        if password and not password == instance.password:
+            hashed_password = await PasswordHandler.hash_password(
+                password
+            )
+            data['password'] = hashed_password
+
+        return await super().update(instance, **data)
+
