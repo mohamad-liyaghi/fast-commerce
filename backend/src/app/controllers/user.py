b64e39c98b31b42c54ebf68ecf83a1e780f5315e
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-08-20 14:48:41 +0330

    added: update profile route

--- backend/src/app/controllers/user.py
+++ backend/src/app/controllers/user.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, status
 from src.core.controller import BaseController
+from src.app.models import User
 from uuid import UUID
 
 
@@ -7,11 +8,9 @@ class UserController(BaseController):
     """
     User controller is responsible for handling user CRUD operations.
     """
-    async def retrieve_profile(self, user_uuid: UUID):
-        """
-        Retrieve a profile by its uuid.
-        """
-        user = await self.retrieve(uuid=user_uuid)
+
+    async def get_by_uuid(self, uuid: UUID):
+        user = await super().get_by_uuid(uuid)
 
         if not user:
             raise HTTPException(
@@ -20,3 +19,23 @@ class UserController(BaseController):
             )
 
         return user
+
+    async def update_user(
+            self,
+            uuid: UUID,
+            requesting_user: User,
+            **kwargs
+    ) -> User:
+        """
+        Update the user's profile if they have permission.
+        """
+        user = await self.get_by_uuid(uuid)
+
+        if user != requesting_user:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to update this profile."
+            )
+
+        return await super().update(user, **kwargs)
+
---
+++ backend/tests/routes/v1/profile/test_update.py
@@ -0,0 +1,34 @@
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+class TestUpdateProfileRoute:
+
+    @pytest.fixture(autouse=True)
+    def setup_method(self, client: AsyncClient, user) -> None:
+        self.client = client
+        self.url = f"v1/profile/{user.uuid}"
+        self.data = {
+            "first_name": "updated first name",
+            "last_name": "updated last name",
+        }
+
+    @pytest.mark.asyncio
+    async def test_update_unauthorized(self) -> None:
+        response = await self.client.put(self.url, json=self.data)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    @pytest.mark.asyncio
+    async def test_update(self, authorized_client) -> None:
+        response = await authorized_client.put(self.url, json=self.data)
+        assert response.status_code == status.HTTP_200_OK
+
+    @pytest.mark.asyncio
+    async def test_update_another_user(self, authorized_client, admin) -> None:
+        response = await authorized_client.put(
+            f"v1/profile/{admin.uuid}",
+            json=self.data
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
