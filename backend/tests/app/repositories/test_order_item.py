f14185bbbcff8d63116debd9bf1066be1920d00e
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-09-29 13:32:48 +0330

    updated: made order item tests work

--- backend/tests/app/repositories/test_order_item.py
+++ backend/tests/app/repositories/test_order_item.py
@@ -21,12 +21,14 @@ class TestOrderItemRepository:
         assert preparing_order_item.status == OrderItemStatusEnum.DELIVERING
 
     @pytest.mark.asyncio
-    async def test_set_delivering_non_vendor(self, preparing_order_item, admin):
+    async def test_set_delivering_non_vendor_fails(self, preparing_order_item, admin):
         with pytest.raises(VendorRequiredException):
             await self.repository.set_delivering(preparing_order_item, admin)
 
     @pytest.mark.asyncio
-    async def test_set_delivering_delivered_item(self, delivered_order_item, user):
+    async def test_set_delivered_item_to_delivering_fails(
+        self, delivered_order_item, user
+    ):
         with pytest.raises(InappropriateOrderStatus):
             await self.repository.set_delivering(delivered_order_item, user)
 
@@ -36,11 +38,14 @@ class TestOrderItemRepository:
         assert delivering_order_item.status == OrderItemStatusEnum.DELIVERED
 
     @pytest.mark.asyncio
-    async def test_set_delivered_non_admin(self, delivering_order_item, user):
+    async def test_set_delivered_non_admin_fails(self, delivering_order_item, user):
         with pytest.raises(AdminRequiredException):
             await self.repository.set_delivered(delivering_order_item, user)
 
     @pytest.mark.asyncio
-    async def test_set_delivered_preparing_item(self, preparing_order_item, admin):
+    async def test_set_delivered_item_to_preparing_fails(
+        self, preparing_order_item, admin
+    ):
+        await self.repository.set_delivered(preparing_order_item, admin)
         with pytest.raises(InappropriateOrderStatus):
             await self.repository.set_delivered(preparing_order_item, admin)
